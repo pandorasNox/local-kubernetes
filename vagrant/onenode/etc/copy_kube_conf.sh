678860d4507603e59475f1ea3ed72a42d7b1423f
Tino Stöckel <tino.stoeckel@turbinekreuzberg.com>
2018-06-19 18:58:22 +0200

    add setup rule in makefile add persistantVolume for kubernetes

--- vagrant/onenode/etc/copy_kube_conf.sh
+++ vagrant/onenode/etc/copy_kube_conf.sh
@@ -2,5 +2,5 @@
 
 mkdir kubernetes || true
 mkdir kubernetes/.kube || true
-scp -i .vagrant/machines/monovagk8s/virtualbox/private_key vagrant@172.17.8.201:/home/vagrant/.kube/config kubernetes/.kube/config
+scp -i .vagrant/machines/monovagk8s/virtualbox/private_key -o StrictHostKeyChecking=no vagrant@172.17.8.201:/home/vagrant/.kube/config kubernetes/.kube/config
 kubectl --kubeconfig kubernetes/.kube/config config set-cluster kubernetes --server=https://172.17.8.201:6443
